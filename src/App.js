fc75c0c050dfd6c7548ee451c833ca4f3b43ecbf
AnsariShahalam <shahalamhus111@gmail.com>
2023-11-10 12:52:50 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "./List/NavBar";
+import CreateList from "./List/CreateList";
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <CreateList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/List/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+const NavBar = ()=>{
+    
+    return(
+        <div className="gray">
+            <nav className="between"> 
+                <p className="welcon" id="User">Welcome User</p>
+                <button className="logout">Logout</button>
+            </nav>
+
+        </div>
+    );
+}
+export default NavBar;
